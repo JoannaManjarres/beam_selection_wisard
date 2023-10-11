e4635387c4c098b4353c6a6d6ac0b58e9a688df4
Joanna Manjarres <joannamanjarres@MacBook-Pro-de-Joanna.local>
2023-10-11 19:07:20 -0300

    comite usando a media do score

--- code/comites.py
+++ code/comites.py
@@ -251,8 +251,89 @@ def committe_using_wisard():
     a=0
 
 
+def committe_using_score_mean():
+    index_beam_combined_train, index_beam_combined_test = read_labels()
+
+    coord_train, coord_test = read_coordinates()
+    print('selecionando feixes a partir das coordenadas')
+    print('   coord train: ', coord_train.shape)
+    beam_selection_for_coord = beam_selection_commite(x_train=coord_train,
+                                                      x_test=coord_test,
+                                                      y_train=index_beam_combined_train,
+                                                      y_test=index_beam_combined_test,
+                                                      addressSize=44)
+
+    lidar_train, lidar_test = read_Lidar()
+    print('selecionando feixes a partir do Lidar')
+    print('   Lidar train: ', lidar_train.shape)
+    beam_selection_for_lidar = beam_selection_commite(x_train=lidar_train,
+                                                      x_test=lidar_test,
+                                                      y_train=index_beam_combined_train,
+                                                      y_test=index_beam_combined_test,
+                                                      addressSize=64)
+
+    committe_score_mean(beam_selection_for_coord, beam_selection_for_lidar, label_validation=index_beam_combined_test)
+def committe_score_mean(beam_selection_for_coord, beam_selection_for_lidar, label_validation):
+
+    # number_of_samples = beam_selection_for_coord.shape[0]
+    number_of_samples = len(beam_selection_for_coord)
 
 
-#committe_using_wisard()
+    beam_selection = []
 
-committe_majority_vote()
+    for i in range(number_of_samples):
+        beam_coord = beam_selection_for_coord[i]
+        beam_lidar = beam_selection_for_lidar[i]
+
+        #max_degree_coord = max(beam_coord, key=itemgetter('degree'))
+        max_degree_coord = max([d['degree'] for d in beam_coord if 'degree' in d])
+        max_degree_lidar = max([d['degree'] for d in beam_lidar if 'degree' in d])
+
+        coord_degree_norm = [d['degree']/max_degree_coord for d in beam_coord if 'degree' in d]
+        lidar_degree_norm = [d['degree']/max_degree_lidar for d in beam_lidar if 'degree' in d]
+
+        # Adiciona a ponderacao no dicionario
+        for j in range(len(beam_coord)):
+            beam_coord[j]['norm'] = coord_degree_norm[j]
+            beam_lidar[j]['norm'] = lidar_degree_norm[j]
+
+
+        # extrai todas as classes que estao no dicionario
+        list_of_class_of_beam_coord = [d['class'] for d in beam_coord if 'class' in d]
+
+        list_of_classes = []
+        list_of_classes_degree = []
+        list_of_classes_degree_1=[]
+        for i in range(len(beam_lidar)):
+            for index, value in enumerate(list_of_class_of_beam_coord):
+                if beam_lidar[i]['class'] == value:
+                    mean_of_degree_norm_per_class = (beam_coord[index]['norm'] + beam_lidar[i]['norm'])/2
+                    classe = beam_lidar[i]['class']
+                    list_of_classes_degree.append([classe, mean_of_degree_norm_per_class])
+                    list_of_classes_degree_1.append((classe, mean_of_degree_norm_per_class))
+
+        order_list_of_classes = sorted(list_of_classes_degree, key=itemgetter(1), reverse=True)
+        #order_list_of_classes = sorted(list_of_classes, key=itemgetter(1), reverse=True)
+
+
+        list_of_classes = np.array(order_list_of_classes)[:,0]
+
+        beam_selection.append(list_of_classes)
+
+    beam_selection_by_comitte = np.array(beam_selection)[:,0]
+    #pro top-k
+    #beam_selection_by_comitte = np.array(beam_selection)[:,0:5]
+
+    acuracia = accuracy_score(label_validation, beam_selection_by_comitte)
+
+    return beam_selection
+
+
+    a=0
+
+
+
+committe_using_score_mean()
+
+
+
