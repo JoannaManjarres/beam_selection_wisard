ce11cc47130e20f795b47dc6b973adf8efdcb064
Joanna Manjarres <joannamanjarres@MacBook-Pro-de-Joanna.local>
2023-11-16 18:22:52 -0300

    implemtacao do comite que escolhe o maior score

--- code/comites.py
+++ code/comites.py
@@ -225,7 +225,8 @@ def committe():
     #accuracy_position_geometric_mean, name_position_geometric_mean = committe_position_geometric_mean(beam_selection_for_coord, beam_selection_for_lidar, label_validation=index_beam_combined_test)
     #accuracy_difference_between_position, name_difference_between_position = committe_difference_between_position(beam_selection_for_coord, beam_selection_for_lidar, label_validation=index_beam_combined_test)
     #accuracy_position_add, name_position_add = committe_position_add(beam_selection_for_coord, beam_selection_for_lidar, label_validation=index_beam_combined_test)
-    accuracy_position_dot, name_position_dot = committe_position_dot(beam_selection_for_coord, beam_selection_for_lidar, label_validation=index_beam_combined_test)
+    #accuracy_position_dot, name_position_dot = committe_position_dot(beam_selection_for_coord, beam_selection_for_lidar, label_validation=index_beam_combined_test)
+    accuracy_bigest_score, name_bigest_score = committe_bigger_score(beam_selection_for_coord, beam_selection_for_lidar, label_validation=index_beam_combined_test)
 
 
 
@@ -1044,6 +1045,89 @@ def committe_score_geometric_mean(beam_selection_for_coord, beam_selection_for_l
 
 
 
+def committe_bigger_score(beam_selection_for_coord, beam_selection_for_lidar, label_validation):
+
+    #number_of_samples = beam_selection_for_coord.shape[0]
+    number_of_samples = len(beam_selection_for_coord)
+    #number_of_classes_to_evaluated = 12
+
+    beam_selection =[]
+
+    for i in range(number_of_samples):
+        beam_coord = beam_selection_for_coord[i]
+        #beam_coord = beam_coord[0:number_of_classes_to_evaluated]
+
+        beam_lidar = beam_selection_for_lidar[i]
+        #beam_lidar = beam_lidar[0:number_of_classes_to_evaluated]
+
+        #Calcula a ponderacao dos degree em cada vetor de feixes selecionados
+        max_degree_coord = max([d['degree'] for d in beam_coord if 'degree' in d])
+        max_degree_lidar = max([d['degree'] for d in beam_lidar if 'degree' in d])
+
+        coord_degree_norm = [d['degree']/max_degree_coord for d in beam_coord if 'degree' in d]
+        lidar_degree_norm = [d['degree']/max_degree_lidar for d in beam_lidar if 'degree' in d]
+
+        #Adiciona a ponderacao no dicionario
+        for j in range(len(beam_coord)):
+            beam_coord[j]['norm'] = coord_degree_norm[j]
+            beam_lidar[j]['norm'] = lidar_degree_norm[j]
+
+        # extrai todas as classes que estao no dicionario
+        list_of_class_of_beam_coord = [int(d['class']) for d in beam_coord if 'class' in d]
+        list_of_class_of_beam_lidar = [int(d['class']) for d in beam_lidar if 'class' in d]
+
+        list_of_classes = []
+        list_of_classes_degree = []
+        list_of_classes_degree_1 = []
+        for i in range(len(list_of_class_of_beam_lidar)):
+            for index, value in enumerate(list_of_class_of_beam_coord):
+                # if beam_lidar[i]['class'] == value:
+                if int(beam_lidar[i]['class']) == value:
+                    if beam_coord[index]['norm'] > beam_lidar[i]['norm']:
+                        list_of_classes.append([value, beam_coord[index]['norm']])
+                    else:
+                        list_of_classes.append([value, beam_lidar[i]['norm']])
+
+                    #diferenca = abs(beam_coord[index]['norm'] - beam_lidar[i]['norm'])
+                    #list_of_classes.append([value, diferenca])
+                    # list_of_classes.append([beam_lidar[i]['class'], diferenca])
+        order_list_of_classes = sorted(list_of_classes, key=itemgetter(1), reverse=True)
+
+        list_of_classes = np.array(order_list_of_classes)[:, 0]
+
+        beam_selection.append(list_of_classes)
+
+    # pro top-k
+    # beam_selection_by_comitte = np.array(beam_selection)[:,0:5]
+
+    # beam_selection_int = [int(i) for i in beam_selection[:][0]]
+    beam_selection_by_comitte = np.array(beam_selection)[:, 0]
+    label_validation_int = [int(i) for i in label_validation]
+    acuracia_1 = accuracy_score(label_validation_int, beam_selection_by_comitte)
+
+    top_k = [1, 5, 10, 20, 30, 40, 50]
+
+    acuracia = []
+
+    for i in range(len(top_k)):
+        acerto = 0
+        nao_acerto = 0
+        for amostra_a_avaliar in range(len(beam_selection)):
+            group = np.array(beam_selection)[:, 0:top_k[i]]
+            if (label_validation_int[amostra_a_avaliar] in group[amostra_a_avaliar]):
+                acerto = acerto + 1
+            else:
+                nao_acerto = nao_acerto + 1
+
+        acuracia.append(acerto / len(beam_selection))
+
+    name_committe = 'maior_do_score'
+    df_acuracia_comite_top_k = pd.DataFrame(acuracia)
+    path = '../results/accuracy/8X32/committe/'
+    df_acuracia_comite_top_k.to_csv(path + 'acuracia_comite_' + name_committe + '_top_k.csv')
+
+    return acuracia, name_committe
+
 
 def plot_results_each_committe( y_data, name_committe):
 
@@ -1134,6 +1218,12 @@ def plot_results_from_csv():
     data_of_score_dot = data.to_numpy()
     name_committe_dot_score = 'Score: produto'
 
+    file_score_biggest = 'acuracia_comite_maior_do_score_top_k'
+    file = path + file_score_dot + '.csv'
+    data = pd.read_csv(file, usecols=[1])
+    data_of_score_biggets = data.to_numpy()
+    name_committe_biggest_score = 'Score: o Maior'
+
 
     sns.set()
 
@@ -1142,7 +1232,11 @@ def plot_results_from_csv():
 
     title_figure = "comparacao de Desempenho de comites "
 
-    plt.plot(top_k, data_of_postion_committe, color='g', linestyle='solid',
+    plt.plot(top_k, data_of_score_biggets, color='lightcoral', linestyle='solid',
+             label=name_committe_biggest_score, linewidth=8, alpha=.5)  # linestyle = 'solid'
+    plt.plot(top_k, data_of_score_committe, 'ro')
+
+    plt.plot(top_k, data_of_postion_committe, color='g', linestyle='dashed',
              label=name_committe_1)  # linestyle = 'dashed' 'dotted'
     plt.plot(top_k, data_of_postion_committe, 'go')
 
@@ -1164,8 +1258,8 @@ def plot_results_from_csv():
     plt.plot(top_k, data_of_score_add, 'b-')
 
     plt.plot(top_k, data_of_score_dot, color='m', linestyle='dashed',
-             label=name_committe_dot_score)  # linestyle = 'solid'
-    plt.plot(top_k, data_of_score_dot, 'mo')
+             label=name_committe_dot_score, linewidth=.8)  # linestyle = 'solid'
+    #plt.plot(top_k, data_of_score_dot, 'mo')
 
     for x, y in zip(top_k, data_of_postion_committe):
         label = "{:.2f}".format(y[0])
@@ -1437,9 +1531,13 @@ def plot_results_in_bars_from_csv_score():
     plt.show()
 
     a=0
+
+
+
 #committe()
+plot_results_from_csv()
 #plot_results_in_bars_from_csv_position()
-plot_results_in_bars_from_csv_score()
+#plot_results_in_bars_from_csv_score()
 
 
 
